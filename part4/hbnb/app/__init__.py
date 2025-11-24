f2e832ca6f8d678aa2ef84915abd4e43611122a1
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-24 11:06:20 +0100

    add: add the prefix to the api that now start the application directly on the index page

--- part4/hbnb/app/__init__.py
+++ part4/hbnb/app/__init__.py
@@ -24,28 +24,34 @@ def create_app(config_class="config.DevelopmentConfig"):
     api = Api(app, version='1.0',
               title='HBnB API',
               description='HBnB Application API',
-              doc='/api/v1/')
+              doc='/api/v1/',
+              prefix='/api/v1')
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
 
     # Register the users namespace
-    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(users_ns, path='/users')
 
     # Register the amenities namespace
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(amenities_ns, path='/amenities')
 
     # Register the places namespace
-    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(places_ns, path='/places')
 
     # Register the reviews namespace
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(reviews_ns, path='/reviews')
 
     # Register the auth namespace
-    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(auth_ns, path='/auth')
 
     # Register the protected namespace
-    api.add_namespace(protected_ns, path='/api/v1/protected')
+    api.add_namespace(protected_ns, path='/protected')
+
+    @app.route("/")
+    def home():
+        return redirect(url_for('index'))
+
 
     @app.route("/index")
     def index():
